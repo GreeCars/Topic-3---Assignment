262bc2f9ca585a386cd1d4aaee4ca2b28993f48b
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-13 09:37:56 -0400

    Working

--- Topic 3 - Assignment/Program.cs
+++ Topic 3 - Assignment/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Carsen
+int roomNumber, graduationYear;
+double price, radius;
+string courseName, fullName;
+
+roomNumber = 29;
+graduationYear = 2026;
+price = 2.99;
+radius = 3;
+courseName = "Computer Science";
+fullName = "Carsen Greenwood";
+
+Console.WriteLine("This is room #" + roomNumber);
+Console.WriteLine("The price is $" + price);
+Console.WriteLine("I am learning a bit about " + courseName + "!");
+Console.WriteLine();
+
+Console.WriteLine("My name is " + fullName + " and I'll graduate in " + graduationYear + ".");
+Console.WriteLine();
+
+Console.WriteLine("The area of a circle with a radius of 3 centimeters is " + ((22.0 / 7.0) * radius * radius) + " centimeters.");
