838306e6f06242c33ee282f9d32b760479c3a234
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-16 09:21:14 -0400

    Finished

--- Topic 3 - Assignment/Program.cs
+++ Topic 3 - Assignment/Program.cs
@@ -1,13 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Carsen
-int roomNumber, graduationYear;
-double price, radius;
+int roomNumber, graduationYear, rectangleWidth;
+double price, radius, area, rectangleLength;
 string courseName, fullName;
 
 roomNumber = 29;
 graduationYear = 2026;
+rectangleWidth = 10;
 price = 2.99;
 radius = 3;
+area = ((Math.PI) * radius * radius);
+rectangleLength = 20.5;
 courseName = "Computer Science";
 fullName = "Carsen Greenwood";
 
@@ -19,4 +22,8 @@ Console.WriteLine();
 Console.WriteLine("My name is " + fullName + " and I'll graduate in " + graduationYear + ".");
 Console.WriteLine();
 
-Console.WriteLine("The area of a circle with a radius of 3 centimeters is " + ((22.0 / 7.0) * radius * radius) + " centimeters.");
+Console.WriteLine("The area of a circle with a radius of 3 centimeters is " + Math.Round(area, 1) + " centimeters.");
+Console.WriteLine();
+
+Console.WriteLine("The area of a rectangle with a width of " + rectangleWidth + "cm and a length of " + rectangleLength + "cm is " + (rectangleLength * rectangleWidth) + "cm.");
+Console.ReadLine();
